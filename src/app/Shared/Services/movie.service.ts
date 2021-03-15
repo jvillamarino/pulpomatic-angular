1d12c1fbf746e38fe100f16612ef381a59472785
Jesus Villamarin <jesus.villamarin@zinobe.com>
2021-03-14 21:36:11 -0500

    Service: Integrate Moviedb service * Create interceptor for add token * Create interface to movie * Create service to get popular movies * Add enviroments * Add Toastr library

---
+++ src/app/Shared/Services/movie.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Movie } from '@Shared/Models/Interfaces/general.interface';
+import { environment as env } from '@Env/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  constructor(private _http: HttpClient) {}
+
+  public getMovies(): Observable<any> {
+    return this._http.get<Movie[]>(
+      `${env.api}/movie/popular?language=en-ES&page=1`
+    );
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api: 'https://api.themoviedb.org/3/',
+  token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGQ4MDY0OGZhNDdkODA5NmMzY2RjMGJkZGYxYTM3NCIsInN1YiI6IjYwNGMxZDkxZGQ0NzE2MDA1MzhkNzIxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.T2_jpNyxnA0gxujlIydWtap5aVECLSrwhVc142F6wbo',
 };
 
 /*
