39f6cd5924d0f0fccc61e2b976107d26fc4fa359
Jesus Villamarin <jesus.villamarin@zinobe.com>
2021-03-16 07:46:28 -0500

    Header: Modify component to show top rate movie

--- src/app/Shared/components/header/header.component.ts
+++ src/app/Shared/components/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Movie } from '@Shared/Models/Interfaces/general.interface';
 import { MovieService } from '@Shared/Services/movie.service';
-import { map} from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'header-movie',
@@ -10,23 +11,38 @@ import { map} from 'rxjs/operators';
 })
 export class HeaderComponent implements OnInit {
   private _lastestMovie: Movie;
+  private _currentPath: string;
 
   public get movie(): Movie {
     return this._lastestMovie;
   }
 
-  constructor(private _movieService: MovieService) {}
+  public get currentPath(): string {
+    return this._currentPath;
+  }
+
+  constructor(private _movieService: MovieService, private router: Router) {
+    this.onChangeRoute();
+  }
 
   ngOnInit() {
     this.getTopMovie();
   }
 
   private getTopMovie() {
-    this._movieService.getTopMovie()
-    .pipe(map(({results}) => results.shift()))
-    .subscribe((res) => {
-      console.log(res);
-      this._lastestMovie = res;
-    });
+    this._movieService
+      .getTopMovie()
+      .pipe(map(({ results }) => results.shift()))
+      .subscribe((res) => {
+        this._lastestMovie = res;
+      });
+  }
+
+  private onChangeRoute() {
+    this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this._currentPath = event.url;
+      });
   }
 }
